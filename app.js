236a0e788cc8fcd53d7411919c967b3ca8696063
Ivan Shnurchenko <ivan.shnurchenko@gmail.com>
2019-06-11 11:44:45 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const pm2 = require('pm2');
+const pmx = require('pmx');
+const sentry = require("@sentry/node");
+
+const conf = pmx.initModule();
+
+pm2.Client.launchBus(function (err, bus) {
+    sentry.init({
+        dsn: conf.sentry_dsn,
+    });
+
+    const trackedApps = (conf.apps || "").split(",");
+
+    if (err)
+        return console.error(err);
+    bus.on('log:*', function (type, packet) {
+        if (trackedApps.length && !trackedApps.includes(packet.process.name)) {
+            return false;
+        }
+
+        sentry.withScope((scope) => {
+            const data = JSON.parse(packet.data || "");
+            scope.setUser(data.user || {});
+            scope.setTags(data.tags || {});
+            scope.setExtras(data.extras || {});
+            scope.setFingerprint(data.fingerprint || ["{{ default }}"]);
+            (data.breadcrumbs || []).forEach((b) => {
+                scope.addBreadcrumb(b);
+            });
+            const eventId = sentry.captureMessage(data.msg || "<no message>", data.level || sentry.Severity.Debug);
+        });
+    });
+});
